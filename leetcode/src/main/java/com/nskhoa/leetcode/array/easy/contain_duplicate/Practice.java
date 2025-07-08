d7e9c1914d0f55a1e5458f238a609f885f8d9611
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-07-08 20:51:57 +0700

    update

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/contain_duplicate/Practice.java
@@ -0,0 +1,10 @@
+package com.nskhoa.leetcode.array.easy.contain_duplicate;
+
+public class Practice {
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+
+            return true;
+        }
+    }
+}
