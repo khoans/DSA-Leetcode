f87839ba7e9dffabeba79f7421fb6c71afb37cad
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-07-08 20:44:28 +0700

    update

--- leetcode/src/main/java/com/nskhoa/leetcode/array/easy/is_anagram/IsAnagram.java
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/is_anagram/IsAnagram.java
@@ -1,5 +1,17 @@
 package com.nskhoa.leetcode.array.easy.is_anagram;
 
+/**
+ * Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+ * Example 1:
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * Example 2:
+ * Input: s = "rat", t = "car"
+ * Output: false
+ * Constraints:
+ * 1 <= s.length, t.length <= 5 * 104
+ * s and t consist of lowercase English letters.
+ */
 public class IsAnagram {
     class Solution {
         public boolean isAnagram(String s, String t) {
@@ -8,13 +20,13 @@ public class IsAnagram {
             }
 
             int[] counter = new int[26];
-            for (int i = 0; i < s.length(); i++ ) {
+            for (int i = 0; i < s.length(); i++) {
                 counter[s.charAt(i) - 'a']++;
                 counter[t.charAt(i) - 'a']--;
             }
 
             for (int count : counter) {
-                if (count !=0) {
+                if (count != 0) {
                     return false;
                 }
             }
---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/two_sum/Practice.java
@@ -0,0 +1,12 @@
+package com.nskhoa.leetcode.array.easy.two_sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Practice {
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            return new int[]{};
+        }
+    }
+}
