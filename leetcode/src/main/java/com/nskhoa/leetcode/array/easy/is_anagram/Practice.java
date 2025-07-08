f2d866bcb043de201ddfe713b50c4ee88984a06f
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-07-08 20:38:43 +0700

    update

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/is_anagram/Practice.java
@@ -0,0 +1,9 @@
+package com.nskhoa.leetcode.array.easy.is_anagram;
+
+public class Practice {
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            return true;
+        }
+    }
+}
