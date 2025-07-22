f2e1819fa68330f2f3a83268f1879661a0e27021
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-07-22 13:59:34 +0700

    update

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/string_matching_in_array/Practice.java
@@ -0,0 +1,13 @@
+package com.nskhoa.leetcode.array.easy.string_matching_in_array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Practice {
+    static class Solution {
+        public List<String> stringMatching(String[] words) {
+            List<String> list = new ArrayList<>();
+            return list;
+        }
+    }
+}
---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/string_matching_in_array/StringMatchingInArray.java
@@ -0,0 +1,30 @@
+package com.nskhoa.leetcode.array.easy.string_matching_in_array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringMatchingInArray {
+    static class Solution {
+        public List<String> stringMatching(String[] words) {
+            List<String> list = new ArrayList();
+            for (int i = 0; i < words.length; i++) {
+                for (int j = 0; j < words.length; j++) {
+                    if (i != j) {
+                        if (words[j].contains(words[i])) {
+                            list.add(words[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+            return list;
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] words = new String[]{"leetcode", "et", "code"};
+        Solution sol = new Solution();
+        List<String> strings = sol.stringMatching(words);
+        System.out.println(strings);
+    }
+}
