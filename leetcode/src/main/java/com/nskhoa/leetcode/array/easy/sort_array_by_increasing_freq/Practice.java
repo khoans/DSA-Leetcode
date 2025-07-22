ff588efa09407f842122a1cdef3fa0103f6d7939
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-07-22 13:12:40 +0700

    hello

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/sort_array_by_increasing_freq/Practice.java
@@ -0,0 +1,13 @@
+package com.nskhoa.leetcode.array.easy.sort_array_by_increasing_freq;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Practice {
+    class Solution {
+        public int[] frequencySort(int[] nums) {
+            return new int[]{};
+        }
+    }
+}
