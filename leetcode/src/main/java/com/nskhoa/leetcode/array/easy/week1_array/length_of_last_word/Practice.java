65a38ed65cacdfb4664cd6b4160db3f8a452e8df
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-13 23:51:51 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/week1_array/length_of_last_word/Practice.java
@@ -0,0 +1,9 @@
+package com.nskhoa.leetcode.array.easy.week1_array.length_of_last_word;
+
+public class Practice {
+    class Solution {
+        public int lengthOfLastWord(String s) {
+            return 0;
+        }
+    }
+}
