42fb30f0388fad7e3cc7524963b27611b3ba5151
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-14 14:48:31 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/week1_array/longest_common_prefix/Practice.java
@@ -0,0 +1,9 @@
+package com.nskhoa.leetcode.array.easy.week1_array.longest_common_prefix;
+
+public class Practice {
+    class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            return "";
+        }
+    }
+}
