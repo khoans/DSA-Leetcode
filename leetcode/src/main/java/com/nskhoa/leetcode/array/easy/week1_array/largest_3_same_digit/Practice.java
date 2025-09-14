c31dfd293f0c125419e205d8b4d21275032c9959
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-14 14:33:56 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/week1_array/largest_3_same_digit/Practice.java
@@ -0,0 +1,11 @@
+package com.nskhoa.leetcode.array.easy.week1_array.largest_3_same_digit;
+
+public class Practice {
+
+
+    class Solution {
+        public String largestGoodInteger(String num) {
+            return "";
+        }
+    }
+}
