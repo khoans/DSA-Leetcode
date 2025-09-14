e32f575ea35b2e77878256ca5848b298e8c6dbb2
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-14 14:51:51 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/week1_array/replace_element_with_greatest_element_on_right_side/Practice.java
@@ -0,0 +1,9 @@
+package com.nskhoa.leetcode.array.easy.week1_array.replace_element_with_greatest_element_on_right_side;
+
+public class Practice {
+    class Solution {
+        public int[] replaceElements(int[] arr) {
+            return new int[]{};
+        }
+    }
+}
---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/week1_array/replace_element_with_greatest_element_on_right_side/ReplaceElementWithGreatestElementOnRightSide.java
@@ -0,0 +1,44 @@
+package com.nskhoa.leetcode.array.easy.week1_array.replace_element_with_greatest_element_on_right_side;
+
+/**
+ * 1299. Replace Elements with Greatest Element on Right Side
+ * Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
+ * After doing so, return the array.
+ * Example 1:
+ * Input: arr = [17,18,5,4,6,1]
+ * Output: [18,6,6,6,1,-1]
+ * Explanation:
+ * - index 0 --> the greatest element to the right of index 0 is index 1 (18).
+ * - index 1 --> the greatest element to the right of index 1 is index 4 (6).
+ * - index 2 --> the greatest element to the right of index 2 is index 4 (6).
+ * - index 3 --> the greatest element to the right of index 3 is index 4 (6).
+ * - index 4 --> the greatest element to the right of index 4 is index 5 (1).
+ * - index 5 --> there are no elements to the right of index 5, so we put -1.
+ * Example 2:
+ * Input: arr = [400]
+ * Output: [-1]
+ * Explanation: There are no elements to the right of index 0.
+ * Constraints:
+ * 1 <= arr.length <= 10^4
+ * 1 <= arr[i] <= 10^5
+ */
+public class ReplaceElementWithGreatestElementOnRightSide {
+    class Solution {
+        public int[] replaceElements(int[] arr) {
+            if (arr.length == 1) {
+                arr[0] = -1;
+                return arr;
+            }
+
+            int original = arr[arr.length - 1];
+            for (int i = arr.length - 2; i >= 0; i--) {
+                int maxValue = Math.max(arr[i + 1], original);
+                original = arr[i];
+                arr[i] = maxValue;
+            }
+
+            arr[arr.length - 1] = -1;
+            return arr;
+        }
+    }
+}
