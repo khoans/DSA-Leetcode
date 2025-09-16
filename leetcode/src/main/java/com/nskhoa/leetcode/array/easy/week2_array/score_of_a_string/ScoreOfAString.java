19d20800af29ce963142e85046f2b0e595565b4e
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-16 15:31:28 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/week2_array/score_of_a_string/ScoreOfAString.java
@@ -0,0 +1,54 @@
+package com.nskhoa.leetcode.array.easy.week2_array.score_of_a_string;
+
+/*
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+Return the score of s.
+
+
+
+Example 1:
+
+Input: s = "hello"
+
+Output: 13
+
+Explanation:
+
+The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+
+Example 2:
+
+Input: s = "zaz"
+
+Output: 50
+
+Explanation:
+
+The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+
+
+Constraints:
+
+2 <= s.length <= 100
+s consists only of lowercase English letters.
+ * */
+public class ScoreOfAString {
+    class Solution {
+        public int scoreOfString(String s) {
+            int score = 0;
+
+            for (int i = 0; i < s.length() - 1; i++) {
+                score += Math.abs(s.charAt(i) - s.charAt(i + 1));
+            }
+            return score;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new ScoreOfAString().new Solution();
+        System.out.println(solution.scoreOfString("hello")); // 13
+        System.out.println(solution.scoreOfString("zaz")); // 50
+    }
+}
---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/week2_array/score_of_a_string/Solution.java
@@ -0,0 +1,7 @@
+package com.nskhoa.leetcode.array.easy.week2_array.score_of_a_string;
+
+public class Solution {
+    public int scoreOfString(String s) {
+        return 0;
+    }
+}
