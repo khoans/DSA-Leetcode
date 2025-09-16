cd114c598b0fbf9d547703c5cea1aee23210c039
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-16 14:51:36 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/week2_array/longest_common_prefix/Solution.java
@@ -0,0 +1,8 @@
+package com.nskhoa.leetcode.array.easy.week2_array.longest_common_prefix;
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+
+}
