dd33938515e37451048984e058a91515a19c62fe
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-18 23:05:21 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/week2_array/divide_array_into_equal_pairs/DivideArrayIntoEqualPairs.java
@@ -0,0 +1,62 @@
+package com.nskhoa.leetcode.array.easy.week2_array.divide_array_into_equal_pairs;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+You are given an integer array nums consisting of 2 * n integers.
+
+You need to divide nums into n pairs such that:
+
+Each element belongs to exactly one pair.
+The elements present in a pair are equal.
+Return true if nums can be divided into n pairs, otherwise return false.
+
+
+
+Example 1:
+
+Input: nums = [3,2,3,2,2,2]
+Output: true
+Explanation:
+There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
+If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions.
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Explanation:
+There is no way to divide nums into 4 / 2 = 2 pairs such that the pairs satisfy every condition.
+
+
+Constraints:
+
+nums.length == 2 * n
+1 <= n <= 500
+1 <= nums[i] <= 500
+* */
+public class DivideArrayIntoEqualPairs {
+    class Solution {
+
+        public boolean divideArray(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+
+            for (int num : nums) {
+                if (set.contains(num)) {
+                    set.remove(num);
+                }
+                else {
+                    set.add(num);
+                }
+            }
+
+            return set.size() == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new DivideArrayIntoEqualPairs().new Solution();
+        System.out.println(solution.divideArray(new int[]{3, 2, 3, 2, 2, 2})); // true
+        System.out.println(solution.divideArray(new int[]{1, 2, 3, 4})); // false
+    }
+}
