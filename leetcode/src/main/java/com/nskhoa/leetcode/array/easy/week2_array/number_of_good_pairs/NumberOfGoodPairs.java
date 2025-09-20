3c69f025b46e97bcdcb543e2d2a68c172e87ef3e
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-20 09:51:28 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/week2_array/number_of_good_pairs/NumberOfGoodPairs.java
@@ -0,0 +1,70 @@
+package com.nskhoa.leetcode.array.easy.week2_array.number_of_good_pairs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+ * */
+
+public class NumberOfGoodPairs {
+    class Solution {
+        public int numIdenticalPairs(int[] nums) {
+            int ans = 0;
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] == nums[j]) {
+                        ans++;
+                    }
+                }
+            }
+
+            return ans;
+        }
+    }
+
+    class Solution2 {
+        public int numIdenticalPairs(int[] nums) {
+            Map<Integer, Integer> counts = new HashMap<>();
+            int ans = 0;
+
+            for (int num : nums) {
+                ans += counts.getOrDefault(num, 0);
+                counts.put(num, counts.getOrDefault(num, 0) + 1);
+            }
+
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new NumberOfGoodPairs().new Solution();
+        int[] nums = {1, 2, 3, 1, 1, 3};
+        System.out.println(solution.numIdenticalPairs(nums)); // 4
+    }
+}
