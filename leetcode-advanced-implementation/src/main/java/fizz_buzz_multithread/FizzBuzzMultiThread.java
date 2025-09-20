a6964e8e7ad746133719b170e73e66aa052ec46a
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-20 11:08:40 +0700

    new

---
+++ leetcode-advanced-implementation/src/main/java/fizz_buzz_multithread/FizzBuzzMultiThread.java
@@ -0,0 +1,162 @@
+package fizz_buzz_multithread;
+
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+/*
+You have the four functions:
+
+printFizz that prints the word "fizz" to the console,
+printBuzz that prints the word "buzz" to the console,
+printFizzBuzz that prints the word "fizzbuzz" to the console, and
+printNumber that prints a given integer to the console.
+You are given an instance of the class FizzBuzz that has four functions: fizz, buzz, fizzbuzz and number. The same instance of FizzBuzz will be passed to four different threads:
+
+Thread A: calls fizz() that should output the word "fizz".
+Thread B: calls buzz() that should output the word "buzz".
+Thread C: calls fizzbuzz() that should output the word "fizzbuzz".
+Thread D: calls number() that should only output the integers.
+Modify the given class to output the series [1, 2, "fizz", 4, "buzz", ...] where the ith token (1-indexed) of the series is:
+
+"fizzbuzz" if i is divisible by 3 and 5,
+"fizz" if i is divisible by 3 and not 5,
+"buzz" if i is divisible by 5 and not 3, or
+i if i is not divisible by 3 or 5.
+Implement the FizzBuzz class:
+
+FizzBuzz(int n) Initializes the object with the number n that represents the length of the sequence that should be printed.
+void fizz(printFizz) Calls printFizz to output "fizz".
+void buzz(printBuzz) Calls printBuzz to output "buzz".
+void fizzbuzz(printFizzBuzz) Calls printFizzBuzz to output "fizzbuzz".
+void number(printNumber) Calls printnumber to output the numbers.
+
+
+Example 1:
+
+Input: n = 15
+Output: [1,2,"fizz",4,"buzz","fizz",7,8,"fizz","buzz",11,"fizz",13,14,"fizzbuzz"]
+Example 2:
+
+Input: n = 5
+Output: [1,2,"fizz",4,"buzz"]
+
+
+Constraints:
+
+1 <= n <= 50
+* */
+public class FizzBuzzMultiThread {
+    private int n;
+    private Semaphore fizz = new Semaphore(0);
+    private Semaphore buzz = new Semaphore(0);
+    private Semaphore fizzbuzz = new Semaphore(0);
+    private Semaphore num = new Semaphore(1);
+
+    public FizzBuzzMultiThread(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        for (int k = 1; k <= n; k++) {
+            if (k % 3 == 0 && k % 5 != 0) {
+                fizz.acquire();
+                printFizz.run();
+                releaseLock(k + 1);
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        for (int k = 1; k <= n; k++) {
+            if (k % 5 == 0 && k % 3 != 0) {
+                buzz.acquire();
+                printBuzz.run();
+                releaseLock(k + 1);
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        for (int k = 1; k <= n; k++) {
+            if (k % 15 == 0) {
+                fizzbuzz.acquire();
+                printFizzBuzz.run();
+                releaseLock(k + 1);
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        for (int k = 1; k <= n; k++) {
+            if (k % 3 != 0 && k % 5 != 0) {
+                num.acquire();
+                printNumber.accept(k);
+                releaseLock(k + 1);
+            }
+        }
+    }
+
+    public void releaseLock(int n) {
+        if (n % 3 == 0 && n % 5 != 0) {
+            fizz.release();
+        }
+        else if (n % 5 == 0 && n % 3 != 0) {
+            buzz.release();
+        }
+        else if (n % 15 == 0) {
+            fizzbuzz.release();
+        }
+        else {
+            num.release();
+        }
+    }
+
+    public static void main(String[] args) {
+        FizzBuzzMultiThread fizzBuzz = new FizzBuzzMultiThread(15);
+
+        Thread threadA = new Thread(() -> {
+            try {
+                fizzBuzz.fizz(() -> System.out.print("fizz, "));
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadB = new Thread(() -> {
+            try {
+                fizzBuzz.buzz(() -> System.out.print("buzz, "));
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadC = new Thread(() -> {
+            try {
+                fizzBuzz.fizzbuzz(() -> System.out.print("fizzbuzz, "));
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadD = new Thread(() -> {
+            try {
+                fizzBuzz.number(number -> System.out.print(number + ", "));
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
+    }
+}
