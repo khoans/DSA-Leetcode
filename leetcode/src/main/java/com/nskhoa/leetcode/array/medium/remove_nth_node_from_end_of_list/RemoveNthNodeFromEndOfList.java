a97cccc699deb5d8a2ea31bfa20765b6053b035e
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-21 22:00:52 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/medium/remove_nth_node_from_end_of_list/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,96 @@
+package com.nskhoa.leetcode.array.medium.remove_nth_node_from_end_of_list;
+
+import com.nskhoa.leetcode.common.ListNode;
+
+/*
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+
+
+Follow up: Could you do this in one pass?
+ * */
+public class RemoveNthNodeFromEndOfList {
+    /*
+    Complexity Analysis
+
+Time complexity : O(L).
+
+The algorithm makes two traversal of the list, first to calculate list length L and second to find the (L−n) th node. There are 2L−n operations and time complexity is O(L).
+
+Space complexity : O(1).
+
+We only used constant extra space.
+     * */
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            int length = 0;
+            ListNode first = head;
+            while (first != null) {
+                length++;
+                first = first.next;
+            }
+            length -= n;
+            first = dummy;
+            while (length > 0) {
+                length--;
+                first = first.next;
+            }
+            first.next = first.next.next;
+            return dummy.next;
+        }
+    }
+
+    /*
+    Complexity Analysis
+
+Time complexity : O(L).
+
+The algorithm makes one traversal of the list of L nodes. Therefore time complexity is O(L).
+
+Space complexity : O(1).
+
+We only used constant extra space.
+     * */
+    class Solution2 {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode first = dummy;
+            ListNode second = dummy;
+            // Advances first pointer so that the gap between first and second is n nodes apart
+            for (int i = 1; i <= n + 1; i++) {
+                first = first.next;
+            }
+            // Move first to the end, maintaining the gap
+            while (first != null) {
+                first = first.next;
+                second = second.next;
+            }
+            second.next = second.next.next;
+            return dummy.next;
+        }
+    }
+}
---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/medium/remove_nth_node_from_end_of_list/Solution.java
@@ -0,0 +1,9 @@
+package com.nskhoa.leetcode.array.medium.remove_nth_node_from_end_of_list;
+
+import com.nskhoa.leetcode.common.ListNode;
+
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return null;
+    }
+}
