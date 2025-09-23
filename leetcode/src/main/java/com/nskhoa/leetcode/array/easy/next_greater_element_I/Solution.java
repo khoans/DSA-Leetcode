37a72bf608d9516f4bf4dec17c9d3db84674ea9f
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-23 10:27:17 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/array/easy/next_greater_element_I/Solution.java
@@ -0,0 +1,7 @@
+package com.nskhoa.leetcode.array.easy.next_greater_element_I;
+
+public class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        return new int[]{};
+    }
+}
