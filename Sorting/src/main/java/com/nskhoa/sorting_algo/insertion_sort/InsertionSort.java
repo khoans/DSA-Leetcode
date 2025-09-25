ad1bb807a6c2b0b0b55abdb0fd920ad95072e3b1
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-09-25 14:49:12 +0700

    new

--- Sorting/src/main/java/com/nskhoa/sorting_algo/insertion_sort/InsertionSort.java
+++ Sorting/src/main/java/com/nskhoa/sorting_algo/insertion_sort/InsertionSort.java
@@ -1,23 +1,19 @@
 package com.nskhoa.sorting_algo.insertion_sort;
 
-public class InsertionSort
-{
+public class InsertionSort {
 
-    // InsertionSort : traverse the array and compare each item with all the i tems on its left and find the
+    // InsertionSort : traverse the array and compare each item with all the items on its left and find the proper place to insert
     // insertion index
-    static void insertionSort(int[] arr)
-    {
+    static void insertionSort(int[] arr) {
         // We leave the first item since it's already sorted
-        for (int i = 1; i < arr.length; i++)
-        {
+        for (int i = 1; i < arr.length; i++) {
             // We temporary store the item that we use to compare its left
             int temp = arr[i];
             // Track the index to insert the temp number
             int j = i;
 
             // the j index will keep decreasing till it finds the smallest number in the sorted section
-            while (j > 0 && arr[j - 1] > temp)
-            {
+            while (j > 0 && arr[j - 1] > temp) {
                 // Since we are inserting, we need to shift the current sorted portion to the right
                 arr[j] = arr[j - 1];
                 j--; // Decreasing
@@ -27,16 +23,13 @@ public class InsertionSort
     }
 
 
-    public static void printArray(int[] array)
-    {
-        for (int i = 0; i < array.length; i++)
-        {
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + "  ");
         }
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int[] arr = {
                 4,
                 5,
