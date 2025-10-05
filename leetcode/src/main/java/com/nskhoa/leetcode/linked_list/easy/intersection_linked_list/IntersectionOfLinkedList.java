3ce3baff8b0e03995f34610fb01d49bfc9eefaca
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-10-06 01:17:07 +0700

    new

---
+++ leetcode/src/main/java/com/nskhoa/leetcode/linked_list/easy/intersection_linked_list/IntersectionOfLinkedList.java
@@ -0,0 +1,116 @@
+package com.nskhoa.leetcode.linked_list.easy.intersection_linked_list;
+
+import com.nskhoa.common.ListNode;
+
+/*
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+
+
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+
+Custom Judge:
+
+The inputs to the judge are given as follows (your program is not given these inputs):
+
+intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+listA - The first linked list.
+listB - The second linked list.
+skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+
+
+
+Example 1:
+
+
+Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+Output: Intersected at '8'
+Explanation: The intersected node's value is 8 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [4,1,8,4,5]. From the head of B, it reads as [5,6,1,8,4,5]. There are 2 nodes before the intersected node in A; There are 3 nodes before the intersected node in B.
+- Note that the intersected node's value is not 1 because the nodes with value 1 in A and B (2nd node in A and 3rd node in B) are different node references. In other words, they point to two different locations in memory, while the nodes with value 8 in A and B (3rd node in A and 4th node in B) point to the same location in memory.
+Example 2:
+
+
+Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+Output: Intersected at '2'
+Explanation: The intersected node's value is 2 (note that this must not be 0 if the two lists intersect).
+From the head of A, it reads as [1,9,1,2,4]. From the head of B, it reads as [3,2,4]. There are 3 nodes before the intersected node in A; There are 1 node before the intersected node in B.
+Example 3:
+
+
+Input: intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+Output: No intersection
+Explanation: From the head of A, it reads as [2,6,4]. From the head of B, it reads as [1,5]. Since the two lists do not intersect, intersectVal must be 0, while skipA and skipB can be arbitrary values.
+Explanation: The two lists do not intersect, so return null.
+
+
+Constraints:
+
+The number of nodes of listA is in the m.
+The number of nodes of listB is in the n.
+1 <= m, n <= 3 * 104
+1 <= Node.val <= 105
+0 <= skipA <= m
+0 <= skipB <= n
+intersectVal is 0 if listA and listB do not intersect.
+intersectVal == listA[skipA] == listB[skipB] if listA and listB intersect.
+
+
+Follow up: Could you write a solution that runs in O(m + n) time and use only O(1) memory?
+ * */
+public class IntersectionOfLinkedList {
+    /*
+    Time Complexity: O(N + M), where N and M are the lengths of the two linked lists.
+    Space Complexity: O(1)
+    Explanation: The idea is to use two pointers, one for each linked list. We traverse
+    through the lists, and when we reach the end of one list, we continue from the head
+    of the other list. If the two lists intersect, the pointers will eventually meet at
+    the intersection node. If they do not intersect, both pointers will eventually reach
+    the end (null) at the same time after traversing both lists.
+    * */
+    public class Solution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            ListNode pA = headA;
+            ListNode pB = headB;
+            while (pA != pB) {
+                pA = pA == null
+                     ? headB
+                     : pA.next;
+                pB = pB == null
+                     ? headA
+                     : pB.next;
+            }
+            return pA;
+            // Note: In the case lists do not intersect, the pointers for A and B
+            // will still line up in the 2nd iteration, just that here won't be
+            // a common node down the list and both will reach their respective ends
+            // at the same time. So pA will be NULL in that case.
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new IntersectionOfLinkedList().new Solution();
+        ListNode intersect = new ListNode(8);
+        intersect.next = new ListNode(4);
+        intersect.next.next = new ListNode(5);
+
+        ListNode headA = new ListNode(4);
+        headA.next = new ListNode(1);
+        headA.next.next = intersect;
+
+        ListNode headB = new ListNode(5);
+        headB.next = new ListNode(6);
+        headB.next.next = new ListNode(1);
+        headB.next.next.next = intersect;
+
+        ListNode result = solution.getIntersectionNode(headA, headB);
+        System.out.println(result != null
+                           ? result.val
+                           : "No intersection");
+    }
+}
---
+++ leetcode/src/main/java/com/nskhoa/leetcode/linked_list/easy/intersection_linked_list/Solution.java
@@ -0,0 +1,10 @@
+package com.nskhoa.leetcode.linked_list.easy.intersection_linked_list;
+
+import com.nskhoa.common.ListNode;
+
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return null;
+    }
+
+}
