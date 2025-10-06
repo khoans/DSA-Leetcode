fb405cdfd5c91c7985fa44f388c5620516f205bc
quynhtruong3001 <quynhtruong.31241021316@st.ueh.edu.vn>
2025-10-06 19:59:18 +0700

    new

--- leetcode/src/main/java/com/nskhoa/leetcode/string/easy/valid_parentheses/ValidParentheses.java
+++ leetcode/src/main/java/com/nskhoa/leetcode/string/easy/valid_parentheses/ValidParentheses.java
@@ -97,6 +97,29 @@ public class ValidParentheses {
         }
     }
 
+    class Solution2 {
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    stack.push(')');
+                }
+                else if (c == '{') {
+                    stack.push('}');
+                }
+                else if (c == '[') {
+                    stack.push(']');
+                }
+                else if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+            return stack.isEmpty();
+
+
+        }
+    }
+
     public static void main(String[] args) {
         Solution solution = new ValidParentheses().new Solution();
         System.out.println(solution.isValid("()")); // true
