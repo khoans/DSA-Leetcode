064997685f4378ed1d694c7b6e14a1ee376c2ac3
khoans <khoans1502@gmail.com>
2025-10-21 19:47:40 +0700

    update

--- Sorting/src/main/java/com/nskhoa/sorting_algo/quick_sort/QuickSortPractice.java
+++ Sorting/src/main/java/com/nskhoa/sorting_algo/quick_sort/QuickSortPractice.java
@@ -3,35 +3,12 @@ package com.nskhoa.sorting_algo.quick_sort;
 public class QuickSortPractice {
 
     public static int partition(int[] arr, int start, int end) {
-        int pivot = end;
-        int i = start -1;
-        for (int j = start; j <= end; j++) {
-            if (arr[j] <= arr[pivot]) {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-        return i;
     }
 
     public static void quickSort(int[] arr, int start, int end) {
-        if (start < end) {
-        int pivot = partition(arr,start,end);
-
-        quickSort(arr, start, pivot -1);
-        quickSort(arr, pivot+1, end);
-}
     }
 
     public static void sort(int[] arr) {
-        if (arr == null || arr.length <2) {
-            return;
-        }
-
-        quickSort(arr, 0, arr.length -1);
 
     }
 
