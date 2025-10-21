c86a9fb78808acd0ba576196c9a8df0aff367903
khoans <khoans1502@gmail.com>
2025-10-22 01:49:16 +0700

    update

--- Sorting/src/main/java/com/nskhoa/sorting_algo/bubble_sort/BubbleSortPractice.java
+++ Sorting/src/main/java/com/nskhoa/sorting_algo/bubble_sort/BubbleSortPractice.java
@@ -1,17 +1,9 @@
 package com.nskhoa.sorting_algo.bubble_sort;
 
 public class BubbleSortPractice {
+
+
     static void bubbleSort(int arr[]) {
-        int n = arr.length;
-        for (int i = 0 ; i < n ; i++) {
-            for (int j = 0 ; j < n -i -1 ; j++) {
-                if (arr[j] > arr[j+1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
     }
 
     static void printArray(int arr[]) {
